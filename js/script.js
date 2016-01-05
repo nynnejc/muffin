db9ad627a04d0b577c64cb25793989c4273d8da6
nynnejc <nynnejc@gmail.com>
2016-01-05 14:29:12 +0100

    add_a_new_muffin feature, removed the hardcoded muffins

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-       
-    $(".galleryimage").click(function( event ) {
-        $(".nextimagediv").first().show().removeClass("nextimagediv");
-    });
+    
+    var muffinHTML = '<img class="muffin galleryimage" src="img/muffin2.png">'
+
+    var add_new_muffin = function( event ) {
+    
+    	var width = Math.round(Math.random()* 200) + "px"
+    	var top = Math.round(Math.random()* 800) + "px"
+    	var left = Math.round(Math.random()* 1000) + "px"
+
+        var muffin = $(muffinHTML).css({
+        	width: width,
+    			left: left,
+    			top: top
+        });
+
+        $("#projects").append(muffin);
+    };
+
+    $("#projects").on("click", ".galleryimage", add_new_muffin);
 });
